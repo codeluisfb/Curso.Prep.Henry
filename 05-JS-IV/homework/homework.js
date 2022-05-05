7ac57b10898128b1c940506a6e09306fc78ec005
codeluisfb <luis.botero90@gmail.com>
2022-05-05 09:55:31 -0500

    InicioJsCuatro

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var NewObjeto = {
+    nombre: nombre,
+    edad: edad,
+    meow: function() {
+      return 'Meow!';
+    }
+  }
+    return NewObjeto;
+
 }
 
 
@@ -14,6 +23,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+
 }
 
 function invocarMetodo(objeto, metodo) {
